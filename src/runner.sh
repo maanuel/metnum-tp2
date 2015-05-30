3d78c2f2af4ce12c75dce6c5b30f4ecd86ee0763
Manuel Carrasco <mgcarrasco2012@gmail.com>
2015-05-30 00:06:21 -0300

    Subir script para ejecutar tests

---
+++ src/runner.sh
@@ -0,0 +1,6 @@
+./tp nuestros_tests/knn_test_68.in knn_test_68.out 0
+./tp nuestros_tests/knn_test_171.in knn_test_171.out 0
+./tp nuestros_tests/knn_test_342.in knn_test_342.out 0
+./tp nuestros_tests/knn_test_854.in knn_test_854.out 0
+./tp nuestros_tests/knn_test_1708.in knn_test_1708.out 0
+./tp nuestros_tests/knn_test_2562.in knn_test_2562.out 0
